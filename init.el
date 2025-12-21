f9ecd6917df2f2c83bf3f1df394b1b483ac6872a
Edenir Anschau <edenir.ans@gmail.com>
2025-12-21 18:16:00 -0300

    add denote package

--- init.el
+++ init.el
@@ -7,4 +7,36 @@
 (hl-line-mode nil)
 (set-frame-font "JetBrainsMono Nerd Font Mono Medium 14" nil t)
 
+
+(use-package denote
+  :ensure t
+  :hook (dired-mode . denote-dired-mode)
+  :bind
+  (("C-c n n" . denote)
+   ("C-c n r" . denote-rename-file)
+   ("C-c n l" . denote-link)
+   ("C-c n b" . denote-backlinks)
+   ("C-c n d" . denote-dired)
+   ("C-c n g" . denote-grep))
+  :config
+  (setq denote-directory (expand-file-name "~/Documents/notes/"))
+
+  (denote-rename-buffer-mode 1))
+
+
+
 ;; packages to install: cider, denote, company, paredit, raindbown-delimiters
+
+;; Do I need this?
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(denote)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
