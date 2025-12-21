d064d6179918a27116e64210430a1ffecadc72c9
Edenir Anschau <edenir.ans@gmail.com>
2025-12-21 18:19:55 -0300

    display line numbers

--- init.el
+++ init.el
@@ -2,11 +2,12 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
-(load-theme 'modus-vivendi-tinted)
 (tool-bar-mode -1)
-(hl-line-mode nil)
-(set-frame-font "JetBrainsMono Nerd Font Mono Medium 14" nil t)
+(global-hl-line-mode 1)
+(global-display-line-numbers-mode 1)
 
+(load-theme 'modus-vivendi-tinted)
+(set-frame-font "JetBrainsMono Nerd Font Mono Medium 14" nil t)
 
 (use-package denote
   :ensure t
